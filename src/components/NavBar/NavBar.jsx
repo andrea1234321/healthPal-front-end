65f075b88d1c6a7dd80abbdb0909465ef6659a7a
Andrea <andrearewards@hotmail.com>
2023-10-19 15:31:01 -0400

    change font, add font-size guides, remove nav links, create outline for landing page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,19 +5,20 @@ const NavBar = ({ user, handleLogout }) => {
   return (
     <nav>
       {user ?
-        <ul>
-          <li>Welcome, {user.name}</li>
-          <li><NavLink to="/profiles">Profiles</NavLink></li>
-          <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
-          <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
-          <li><NavLink to="/chat/results">Results</NavLink></li>
-          <li><NavLink to="/chat/concern">Concern</NavLink></li>
+        <ul className='navLinks'>
+          <li><NavLink to="">Health Pal</NavLink></li>
+          {/* <li><NavLink to="/profiles">Profiles</NavLink></li> */}
+          <li><NavLink to="" onClick={handleLogout}>Log Out</NavLink></li>
+          {/* <li><NavLink to="/auth/change-password">Change Password</NavLink></li> */}
+          {/* <li><NavLink to="/chat/results">Results</NavLink></li> */}
+          {/* <li><NavLink to="/chat/concern">Concern</NavLink></li> */}
         </ul>
       :
-        <ul>
-          <li><NavLink to="/auth/login">Log In</NavLink></li>
+        <ul className='navLinks'>
+          <li><NavLink to="">Health Pal</NavLink></li>
+          {/* <li><NavLink to="/auth/login">Log In</NavLink></li>
           <li><NavLink to="/auth/signup">Sign Up</NavLink></li>
-          <li><NavLink to="/profiles">Profiles</NavLink></li>
+          <li><NavLink to="/profiles">Profiles</NavLink></li> */}
         </ul>
       }
     </nav>
