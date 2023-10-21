fa3e8474ffa4f650fc0fa70f2379ea62bbd3aefb
Eunice Yang <euniceyang21@gmail.com>
2023-10-21 14:55:33 -0500

    Style nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 // npm modules
 import { NavLink } from 'react-router-dom'
-import logo from '../../../public/logo.svg'
+import logo from '../../assets/icons/logo.svg'
 
 // assets
 import styles from './NavBar.module.css'
@@ -10,27 +10,23 @@ const NavBar = ({ user, handleLogout, profile}) => {
   return (
     <nav>
       {user ?
-        <ul className='navLinks'>
-          <li className='logoName'>
-            <NavLink to="/">
-              <img src={logo} alt="Health Pal Logo" />
-              Health Pal
-            </NavLink>
-          </li>
-          <li className='logout'><NavLink to="" onClick={handleLogout}>Log Out </NavLink></li>
+        <ul className='navLinks private'>
           <li><NavLink to=""><img className='photo'src={profile.photo} alt="My Photo" /></NavLink></li>
-          
-          
+
+          <li className='logoName'><NavLink to="/"><img src={logo} alt="Health Pal Logo" />Health Pal</NavLink>
+          </li>
+
+          <li className='logout'><NavLink to="" onClick={handleLogout}>Log Out </NavLink></li>
           {/* <li><NavLink to="" onClick={handleLogout}>Log Out</NavLink></li> */}
           {/* <li><NavLink to="/profiles">Profiles</NavLink></li> */}
           {/* <li><NavLink to="/auth/change-password">Change Password</NavLink></li> */}
           {/* <li><NavLink to="/chat/results">Results</NavLink></li> */}
           {/* <li><NavLink to="/chat/concern">Concern</NavLink></li> */}
         </ul>
-        
       :
-        <ul className='navLinks'>
-          <li><NavLink to="">Health Pal</NavLink></li>
+        <ul className='navLinks public'>
+          <li className='logoName'><NavLink to="/"><img src={logo} alt="Health Pal Logo" />Health Pal</NavLink>
+          </li>
           {/* <li><NavLink to="/auth/login">Log In</NavLink></li>*/}
           {/* <li><NavLink to="/auth/signup">Sign Up</NavLink></li> */}
         </ul>
